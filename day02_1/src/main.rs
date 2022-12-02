fa6a219a7dd271987809ff20c0a4d8c6b3c5e437
Tim Jörgen <tim.joergen@googlemail.com>
2022-12-02 13:43:00 +0000

    Day2 closure abuse.

--- day02_1/src/main.rs
+++ day02_1/src/main.rs
@@ -1,26 +1,36 @@
 use std::collections::HashMap;
 
-fn main() {
-    let mut result_hash_map: HashMap<_, _> = HashMap::new();
+fn calculate_result(result_hash_map: HashMap<&str, (u32, u32)>) -> (u32, u32) {
+    let mut sums = (0, 0);
 
-    result_hash_map.insert("A X", 4);
-    result_hash_map.insert("A Y", 8);
-    result_hash_map.insert("A Z", 3);
-    result_hash_map.insert("B X", 1);
-    result_hash_map.insert("B Y", 5);
-    result_hash_map.insert("B Z", 9);
-    result_hash_map.insert("C X", 7);
-    result_hash_map.insert("C Y", 2);
-    result_hash_map.insert("C Z", 6);
-
-    let result: u32 = include_str!("../input.txt")
+    include_str!("../input.txt")
         .lines()
         .map(|line_score| {
-            return result_hash_map.get(line_score).unwrap();
+            sums.0 += result_hash_map.get(line_score).unwrap().0;
+            sums.1 += result_hash_map.get(line_score).unwrap().1;
         })
-        .collect::<Vec<_>>()
-        .into_iter()
-        .sum();
+        .collect::<Vec<_>>();
 
-    println!("{}", result);
+    return sums;
+}
+
+fn solve() -> (u32, u32) {
+    let mut result_hash_map: HashMap<_, _> = HashMap::new();
+
+    result_hash_map.insert("A X", (4, 3));
+    result_hash_map.insert("A Y", (8, 4));
+    result_hash_map.insert("A Z", (3, 8));
+    result_hash_map.insert("B X", (1, 1));
+    result_hash_map.insert("B Y", (5, 5));
+    result_hash_map.insert("B Z", (9, 9));
+    result_hash_map.insert("C X", (7, 2));
+    result_hash_map.insert("C Y", (2, 6));
+    result_hash_map.insert("C Z", (6, 7));
+
+    return calculate_result(result_hash_map);
+}
+
+fn main() {
+    println!("part1: {}", solve().0);
+    println!("part1: {}", solve().1);
 }
