cdc5d1aa5f67574022acca3ce0005b6395ffe7b6
Tim Jörgen <tim.joergen@googlemail.com>
2022-12-03 20:44:18 +0000

    Day3 part 1 solution

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,19 +1,25 @@
 
-pub fn solve() -> u32 {
-    // include_str!("../input.txt")
+pub fn part1() -> (u32, u32) {
+    let a_Z = ('a'..='z').chain('A'..='Z').into_iter().collect::<Vec<char>>();
+    let mut final_score: u32 = 0;
 
-    let mut _foo: u32 = 2;
+    for line in include_str!("../input.txt").lines() {
+        let first_compartment_end_position = line.len() / 2;
+        let first_compartment = line.split_at(first_compartment_end_position).0;
+        let second_compartment = line.split_at(first_compartment_end_position).1;
 
-    _foo = 5;
+        let found_char = first_compartment.chars().find(|c| second_compartment.contains(*c)).unwrap();
+        let elf_score = a_Z.iter().position(|&x| x == found_char).unwrap();
+        final_score += elf_score as u32;
+        final_score += 1;
+        // println!("foo {} {} {} {} {} {}", first_compartment_end_position, line, first_compartment, second_compartment, found_char, elf_score);
+    }
 
-    _foo = 6;
-
-    _foo = 7;
-
-    _foo
+    return (final_score, 0);
 }
 
 fn main() {
-    println!("part1: {}", solve());
+    assert_eq!(part1().0, 7691);
+    println!("part2: {}", part1().1);
 }
 
