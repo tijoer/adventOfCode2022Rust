3597eec847b28defba4f30f591ac78f60102a6dd
Tim Jörgen <tim.joergen@googlemail.com>
2022-12-03 21:45:23 +0000

    Added horrible day3 part 2 solution.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,7 +1,9 @@
 
-pub fn part1() -> (u32, u32) {
-    let a_Z = ('a'..='z').chain('A'..='Z').into_iter().collect::<Vec<char>>();
+pub fn solve() -> (u32, u32) {
+    let a_z = ('a'..='z').chain('A'..='Z').into_iter().collect::<Vec<char>>();
     let mut final_score: u32 = 0;
+    let mut part_2_score: u32 = 0;
+    let mut elf_group: Vec::<String> = Vec::new();
 
     for line in include_str!("../input.txt").lines() {
         let first_compartment_end_position = line.len() / 2;
@@ -9,17 +11,51 @@ pub fn part1() -> (u32, u32) {
         let second_compartment = line.split_at(first_compartment_end_position).1;
 
         let found_char = first_compartment.chars().find(|c| second_compartment.contains(*c)).unwrap();
-        let elf_score = a_Z.iter().position(|&x| x == found_char).unwrap();
+        let elf_score = a_z.iter().position(|&x| x == found_char).unwrap();
         final_score += elf_score as u32;
-        final_score += 1;
-        // println!("foo {} {} {} {} {} {}", first_compartment_end_position, line, first_compartment, second_compartment, found_char, elf_score);
+        final_score += 1; // Fuck this, lol :D. Might read the assigment correctly next time.
+
+        elf_group.push(line.to_string());
+
+        let mut match_in_all_three: char;
+        let mut badge_char: char = '0';
+        if elf_group.len() == 3 {
+            let elf_0 = elf_group.get(0);
+            let elf_1 = elf_group.get(1);
+            let elf_2 = elf_group.get(2);
+
+            for c in elf_0.unwrap().chars() {
+                let foo = elf_1.unwrap().chars().find(|x| *x == c);
+
+                if foo != None {
+                    println!("xxx {}", foo.unwrap());
+                    let bar = elf_2.unwrap().chars().find(|x| *x == foo.unwrap());
+                    if bar != None {
+                        println!("xxx MATCH IN ALL 3 {}", bar.unwrap());
+                        badge_char = bar.unwrap();
+                    }
+                } else {
+                    println!("xxx ");
+                }
+            }
+            println!("------");
+
+            let badge_score = a_z.iter().position(|&x| x == badge_char).unwrap();
+            part_2_score += badge_score as u32;
+            part_2_score += 1;
+
+            elf_group.clear();
+        }
     }
 
-    return (final_score, 0);
+    return (final_score, part_2_score);
 }
 
 fn main() {
-    assert_eq!(part1().0, 7691);
-    println!("part2: {}", part1().1);
+    let result = solve();
+    assert_eq!(result.0, 7691);
+    assert_eq!(result.1, 2508);
+    println!("part1: {}", result.0);
+    println!("part2: {}", result.1);
 }
 
