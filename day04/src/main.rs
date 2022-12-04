fb47771e95e25ed6f1fc481072ad42a952417880
tim <tim>
2022-12-04 11:33:18 +0000

    day04 stub

---
+++ day04/src/main.rs
@@ -0,0 +1,19 @@
+
+pub fn solve() -> (u32, u32) {
+    let mut part_1_score: u32 = 0;
+    let mut part_2_score: u32 = 0;
+
+    include_str!("../input.txt")
+
+
+    return (part_1_score, part_2_score);
+}
+
+fn main() {
+    let result = solve();
+    assert_eq!(result.0, 0);
+    assert_eq!(result.1, 0);
+    println!("day4 part1: {}", result.0);
+    println!("day4 part2: {}", result.1);
+}
+
