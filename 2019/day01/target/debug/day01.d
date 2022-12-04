af4caaa834f7fb9fdf86af9c56f12035a1156c69
Tim <tim>
2022-12-04 21:23:57 +0000

    Bug in .gitignore. Deleting target folders again.

--- 2019/day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/workspaces/adventOfCode2022Rust/2019/day01/target/debug/day01: /workspaces/adventOfCode2022Rust/2019/day01/src/main.rs
--- 2019/day02/target/debug/day02.d
+++
@@ -1,1 +0,0 @@
-/workspaces/adventOfCode2022Rust/2019/day02/target/debug/day02: /workspaces/adventOfCode2022Rust/2019/day02/src/main.rs
