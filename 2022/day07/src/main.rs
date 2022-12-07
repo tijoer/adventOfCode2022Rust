3ef4b6b9527365b25f59d0dd3304168e364f3fdd
Tim Jörgen <tim.joergen@googlemail.com>
2022-12-07 15:30:50 +0000

    Day07 part 1 working

--- 2022/day07/src/main.rs
+++ 2022/day07/src/main.rs
@@ -22,6 +22,7 @@ impl Folder {
 
             let commands = line.split(' ').collect::<Vec<&str>>();
             let command = commands.get(1).unwrap().trim();
+            let first_word = commands.get(0).unwrap().trim();
 
             if command.eq("cd") {
                 let subcommand =  commands.get(2).unwrap().trim();
@@ -36,18 +37,43 @@ impl Folder {
                         subfolders: Vec::new()
                     };
                     new_folder.build_tree(lines);
+                    self.size += new_folder.size;
 
                     self.subfolders.push(new_folder);
                 }
-                println!("cd {}", subcommand);
+            }
+
+            if first_word.eq("$") || first_word.eq("dir") {
+                continue
+            } else {
+                self.size += first_word.parse::<i32>().unwrap();
             }
         }
 
         return self;
     }
+
+    pub fn calculate_to_at_most_100000(&self) -> i32 {
+        let mut result = 0;
+        if self.size <= 100000 {
+            result += self.size;
+        }
+
+        result += self.subfolders.iter().map(|f| {
+            return f.calculate_to_at_most_100000()
+        }).sum::<i32>();
+        
+        return result;
+    }
+
+    pub fn find_smallest_folder_size_greater_30000000(&self) -> i32 {
+        let mut result = 0;
+        // TODO
+        return 0;
+    }
 }
 
-fn solve(input: &str) -> i32 {
+fn part1(input: &str) -> i32 {
     let mut lines = input.lines().collect::<Vec<&str>>();
     
     let mut root = Folder {
@@ -57,21 +83,37 @@ fn solve(input: &str) -> i32 {
     };
 
     root.build_tree(&mut lines);
-    dbg!(root);
+    return root.calculate_to_at_most_100000();
+}
 
-    return 0;
+fn part2(input: &str) -> i32 {
+    let mut lines = input.lines().collect::<Vec<&str>>();
+    
+    let mut root = Folder {
+        name: "/".to_string(),
+        size: 0,
+        subfolders: Vec::new(),
+    };
+
+    root.build_tree(&mut lines);
+    return root.find_smallest_folder_size_greater_30000000();
 }
 
 fn main() {
-    println!("day07 part1: {}", solve(include_str!("../input.txt")));
+    println!("day07 part1: {}", part1(include_str!("../input.txt")));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solve;
+    use crate::*;
 
     #[test]
     fn part1_works() {
-        assert_eq!(solve(include_str!("../inputTestData.txt")), 95437);
+        assert_eq!(part1(include_str!("../inputTestData.txt")), 95437);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(include_str!("../inputTestData.txt")), 24933642);
     }
 }
